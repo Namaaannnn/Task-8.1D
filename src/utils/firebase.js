ce8c39727f7e35bfd709a918e89b7a10657b05bf
Namaaannnn <naman4831.be23@chitkara.edu.in>
2024-11-12 22:21:31 +0530

    Add files via upload

---
+++ src/utils/firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBnJ5z969HXPWtwM7d2-TIdSl7g2CL6NMA",
+  authDomain: "devdeakin-16faa.firebaseapp.com",
+  projectId: "devdeakin-16faa",
+  storageBucket: "devdeakin-16faa.firebasestorage.app",
+  messagingSenderId: "851133500553",
+  appId: "1:851133500553:web:257a2fecc67af188d18a58",
+  measurementId: "G-FRWSDWJ7L8"
+};
+
+
+const app = initializeApp(firebaseConfig); 
+
+
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+
+export { db, storage };
